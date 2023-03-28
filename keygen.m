80c3abc7a513b2e31698a00cff754b38d73d7875
Elijah Ki-Zerbo <kizerboeli@gmail.com>
2023-03-28 11:19:50 +0200

    initiated key generator

---
+++ keygen.m
@@ -0,0 +1,23 @@
+function [n, e, d] = keygen(p, q)
+% RSA Key Generation
+%
+% This script generates the public and private keys for the RSA encryption
+% algorithm. The keys are generated based on two prime numbers p and q, and
+% the RSA algorithm parameters e and d are calculated according to the RSA
+% key generation process.
+%
+% Usage:
+%   [n, e, d] = keygen(p, q)
+%
+% Inputs:
+%   p - a prime number
+%   q - a prime number
+%
+% Outputs:
+%   n - the modulus for the public and private keys
+%   e - the public exponent for the public key
+%   d - the private exponent for the private key
+%
+% Written by Elijah Ki-Zerbo
+
+endfunction
